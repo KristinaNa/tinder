f092b6e7866c69b65f94a40e8daf59cd479fbaa7
Kristina <nosik94@list.ru>
2016-01-25 22:07:02 +0200

    random select

---
+++ tinder/__init__.py
@@ -0,0 +1,7 @@
+from tinder.views import LoginFormView
+
+
+def __init__(self, *args, **kwargs):
+        super(LoginFormView, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-signin'
--- tinder/views.py
+++ tinder/views.py
@@ -40,29 +40,24 @@ class Photos(View):
     # template_name = "photos.html"
     def get(self, request):
         current_user = request.user
-        other_users_photos = upload_foto.objects.filter(~Q(user_id = current_user.id)).values()
+        other_users_photos = upload_foto.objects.values('foto').filter(~Q(user_id = current_user.id)).order_by('?').first().values()
         return render_to_response(
             'photos.html',
             { 'other_users_photos': other_users_photos},
            context_instance=RequestContext(request)
         )
 
-
 class List(View):
     def get(self, request):
         form = PhotoForm() # A empty, unbound form
         current_user = request.user
         photos = upload_foto.objects.filter(user_id = current_user.id).values()
-        other_users_photos = upload_foto.objects.all().values()
-
         # Render list page with the documents and the form
         return render_to_response(
             'my_photos.html',
-            {'photos': photos, 'form': form, 'other_users_photos': other_users_photos},
+            {'photos': photos, 'form': form },
             context_instance=RequestContext(request)
         )
-
-
     def post(self, request):
         form = PhotoForm(request.POST, request.FILES)
         if form.is_valid():
