7e7ca421f4d079d926bdd1c21e0982fb1fdfd272
Kristina <nosik94@list.ru>
2016-02-08 23:26:12 +0200

    join

--- tinder/views.py
+++ tinder/views.py
@@ -46,7 +46,15 @@ class Photos(View):
         )
     def post(self, request):
         current_user = request.user
+
+        #SELECT foto_id FROM tinder_rating WHERE user_id = 2     //    [25, 27]
+        #SELECT foto FROM tinder_upload_foto WHERE user_id != 2 AND id NOT IN (25, 27)
+        rated_photos = rating.objects.values('foto_id').filter(user_id = current_user.id)
+        all_photos = upload_foto.objects.values('foto').filter(~Q(user_id = current_user.id)).exclude(rated_photos).order_by('?').first()
+
         other_users_photos = upload_foto.objects.values('foto', 'id').filter(~Q(user_id = current_user.id)).order_by('?').first()
+        #points = SinglePoint.objects.filter(connection__vektordata__order__project__slug=slug)
+        not_rated_photos = rating.objects.values('foto_id').filter(~Q(user_id = current_user.id))
 
         if request.POST.get('photo_id'):
             photo_id = request.POST.get('photo_id')
