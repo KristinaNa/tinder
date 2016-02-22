e7d555aa7a0a4d989c96b6670fb6509cb15d176b
Vitali Raikov <nosik94@list.ru>
2016-02-22 10:19:15 +0200

    hide thumbnails if no photos

--- mytinder/urls.py
+++ mytinder/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 
@@ -8,6 +7,5 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('tinder.urls')),
-    url(r'^%s/' % settings.DAJAXICE_MEDIA_PREFIX, include('dajaxice.urls')),
 
 ]
