fc37c00f0f7b78f8a1834cd2df6b1a9c2587ca43
Vitali Raikov <nosik94@list.ru>
2016-03-13 23:32:20 +0200

    changes

--- mytinder/settings.py
+++ mytinder/settings.py
@@ -132,8 +132,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
- os.path.join(BASE_DIR, "static")
-  '/tinder/static/',
+ os.path.join(BASE_DIR, "static"),
+  '/tinder/static/'
 ]
 
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
