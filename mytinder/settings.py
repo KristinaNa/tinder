afaf5df31f3680580a9496d0ce934e72bb788452
Vitali Raikov <nosik94@list.ru>
2016-03-13 23:08:30 +0200

    changes

--- mytinder/settings.py
+++ mytinder/settings.py
@@ -49,8 +49,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'django.middleware.security.SecurityMiddleware'
 )
 
 ROOT_URLCONF = 'mytinder.urls'
