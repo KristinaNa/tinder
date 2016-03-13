14dc071d15e7285fbc29f62e87130372c71a3a22
Vitali Raikov <nosik94@list.ru>
2016-03-13 23:15:19 +0200

    changes

--- mytinder/settings.py
+++ mytinder/settings.py
@@ -128,11 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/documents/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'documents')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "documents"),
     'documents/',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
